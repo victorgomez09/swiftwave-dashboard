3f1f36b652ae174cf6992ae0ec424945f725eac0
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-07 15:18:13 +0530

    feat: move maintanence page dynamic import to static import

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/pages/LoginView.vue'
+import UnderMaintenancePage from '@/views/pages/UnderMaintenance.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/maintenance',
       name: 'Maintenance',
-      component: () => import('@/views/pages/UnderMaintenance.vue')
+      component: UnderMaintenancePage
     },
     {
       path: '/login',
