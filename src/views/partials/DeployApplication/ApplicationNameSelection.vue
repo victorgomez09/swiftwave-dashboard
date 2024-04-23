25a9d216f31cfe774cee4ed5a980e1dde8c06458
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-23 22:37:46 +0530

    fix: dont disable app name check input to avoid input focus lost (#135)

--- src/views/partials/DeployApplication/ApplicationNameSelection.vue
+++ src/views/partials/DeployApplication/ApplicationNameSelection.vue
@@ -63,7 +63,6 @@ const isExistApplicationNameCheck = () => {
         <input
           id="application_name"
           v-model="newApplicationName"
-          :disabled="isExistApplicationNameLoading"
           @keydown="preventSpaceInput"
           autocomplete="off"
           class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
